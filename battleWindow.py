d795ef625f81f9f8e032b1595109990985f750a3
syu <syukunt@icloud.com>
2021-07-17 19:04:26 +0900

    戦闘時のプログラム設計開始

---
+++ battleWindow.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.locals import *
+
+class Monster:
+    def __init__(self, img_path, HP, level):
+        self.path = img_path
+        self.HP = HP
+        self.level = level
+    
+    def display(self, screen, x, y):
+
+    def attack(self, attak_point):
+        
+

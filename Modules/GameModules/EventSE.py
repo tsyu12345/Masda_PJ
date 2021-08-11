eeee67aed98ac5367f937affe663eb248a954021
syu <syukunt@icloud.com>
2021-08-12 02:28:30 +0900

    カーソルを動かしたときの効果音を追加

--- Modules/GameModules/EventSE.py
+++ Modules/GameModules/EventSE.py
@@ -3,13 +3,14 @@ from pygame.locals import *
 
 class EventSound:
     def __init__(self):
-        self.key_yes = pygame.mixer.Sound('../../sounds/OtherSounds/key_yes.wav')
-        self.key_no = pygame.mixer.Sound('../../sounds/OtherSounds/key_no.wav')
-        self.key_Enter = pygame.mixer.Sound('../../sounds/OtherSounds/key_Enter.wav')
-        self.yes_btn = pygame.mixer.Sound('../../sounds/clickSound/systen40.wav')
-        self.no_btn = pygame.mixer.Sound('../../sounds/clickSound/systen41.wav')
-        self.btn_select = pygame.mixer.Sound('../../sounds/OtherSounds/button_select.wav')
-        self.text_update = pygame.mixer.Sound('../../sounds/OtherSounds/serif.wav')
+        self.key_yes = pygame.mixer.Sound('sounds/OtherSounds/key_yes.wav')
+        self.key_no = pygame.mixer.Sound('sounds/OtherSounds/key_no.wav')
+        self.key_Enter = pygame.mixer.Sound('sounds/OtherSounds/key_Enter.wav')
+        self.yes_btn = pygame.mixer.Sound('sounds/clickSound/systen40.wav')
+        self.no_btn = pygame.mixer.Sound('sounds/clickSound/systen41.wav')
+        self.btn_select = pygame.mixer.Sound('sounds/OtherSounds/button_select.wav')
+        self.text_update = pygame.mixer.Sound('sounds/OtherSounds/serif.wav')
+        self.menu_carsol_move = pygame.mixer.Sound('sounds/OtherSounds/NES-RPG02-06(Select).wav')
     
     def event_catch_se(self, event:pygame.event, btn=None):
         """Use in event for loop of pygame.event.get(). """
@@ -24,7 +25,7 @@ class EventSound:
 class PlayerSound:
     """Definiction of PlayerSound effect"""
     def __init__(self):
-        self.walk = pygame.mixer.Sound('../../sounds/PlaySounds/walk.wav')
-        self.succsess = pygame.mixer.Sound('../../sounds/PlaySounds/succsess.wav')
-        self.damege = pygame.mixer.Sound('../../sounds/PlaySounds/powerdown02.wav')
-        self.levelUP = pygame.mixer.Sound('../../sounds/PlaySounds/levelup.wav')
+        self.walk = pygame.mixer.Sound('sounds/PlaySounds/walk.wav')
+        self.succsess = pygame.mixer.Sound('sounds/PlaySounds/succsess.wav')
+        self.damege = pygame.mixer.Sound('sounds/PlaySounds/powerdown02.wav')
+        self.levelUP = pygame.mixer.Sound('sounds/PlaySounds/levelup.wav')
